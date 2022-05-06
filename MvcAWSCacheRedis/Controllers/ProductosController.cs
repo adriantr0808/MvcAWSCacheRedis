d993c9f460931ba0b0236baad0c27b17e8f9d29c
attiaApp <attiapp123@gmail.com>
2022-05-06 10:56:31 +0200

    Add project files.

---
+++ MvcAWSCacheRedis/Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSCacheRedis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAWSCacheRedis.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private Repositories.RepositoryProductos repo;
+
+        public ProductosController(Repositories.RepositoryProductos repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Producto> prs = this.repo.GetProductos();
+            return View(prs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Producto p = this.repo.FindProducto(id);
+            return View(p);
+        }
+    }
+}
